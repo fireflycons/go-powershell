639b0b209a16df7a807b7130a394e2d9db3d2a93
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2025-09-25 07:53:10 +0100

    Repoint imports and set up go.mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fireflycons/go-powershell
+
+go 1.25.1
+
+require github.com/juju/errors v1.0.0
--- shell.go
+++ shell.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/gorillalabs/go-powershell/backend"
-	"github.com/gorillalabs/go-powershell/utils"
+	"github.com/fireflycons/go-powershell/backend"
+	"github.com/fireflycons/go-powershell/utils"
 	"github.com/juju/errors"
 )
 
